87baa4ceb177b30d32845202af0cc84a4657fafe
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-19 06:50:48 +0300

    add api documentation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,15 @@
 const HttpStatus = require('http-status')
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDocument = require('../../swagger.json');
 const smsRoutes = require('./smsRoutes')
 const contactRoutes = require('./contactRoutes')
 
 const routes = app => {
   app.use('/sms', smsRoutes)
   app.use('/contacts', contactRoutes)
-  app.get('/', (req, res) => res.status(HttpStatus.OK).send('welcome to sms API'))
+  app.get('/', (req, res) => res.status(HttpStatus.OK).send('welcome to sms API, visit /api-docs for documentation'))
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   //any route apart from the above defined
   app.use('*', (req, res) => res.status(HttpStatus.NOT_FOUND).send('path not found'))
   return app
